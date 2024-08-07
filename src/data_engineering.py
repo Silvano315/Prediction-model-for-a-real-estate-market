a26981471ecb6a0b0eef0bbdec6ad12b4e4b1815
Silvano Quarto <silvano.quarto@gmail.com>
2024-08-07 13:20:22 +0200

    fx integrated

--- src/data_engineering.py
+++ src/data_engineering.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
 
 # Function to check if there are duplicates in the dataset
 def handle_duplicates(df):
@@ -14,3 +16,19 @@ def handle_duplicates(df):
         df_cleaned = df
 
     return df_cleaned
+
+# Function to perform EDA with plotly.express: bar plot for categorical features and histogram for numerical 
+def plot_feature_distribution(df, feature, is_categorical):
+
+    if is_categorical:
+        value_counts = df[feature].value_counts().reset_index()
+        value_counts.columns = [feature, 'count']
+        fig = px.bar(value_counts,
+                     x=feature, y='count',
+                     labels={feature: feature, 'count' : 'Count'},
+                     title=f'Distribution of {feature}')
+    else:
+        fig = px.histogram(df, x = feature, labels={feature:feature},
+                           marginal='box', #'violin'
+                           title=f'Distribution of {feature}')
+    fig.show()
