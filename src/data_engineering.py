5fcd46746bb7a6165121e96a1ec82cc01e232d52
Silvano Quarto <silvano.quarto@gmail.com>
2024-08-07 17:35:23 +0200

    fx updated

--- src/data_engineering.py
+++ src/data_engineering.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 
 # Categorical features with type int64 conversion to 'category'
 def convert_to_category(df, features):
@@ -51,3 +53,41 @@ def plot_feature_distribution(df, feature, is_categorical, comparison = None):
                            marginal='box', #'violin'
                            title=f'Distribution of {feature}')
     fig.show()
+
+
+# Function to do One-Hot Encoding on the categorical features
+def apply_encoding(df):
+
+    cat_features = df.select_dtypes(include='category').columns
+    encoding_features = [feature for feature in cat_features if df[feature].nunique() > 2]
+
+    df_encoded = pd.get_dummies(df, columns=encoding_features, drop_first=True, dtype=int)
+
+    return df_encoded
+
+# Function to do Feature Scaling with standardization
+def feature_scaling_standardization(X_train, X_test):
+
+    continuous_features = ['area'] 
+
+    continuous_transformer = Pipeline(
+        steps=[('scaler', StandardScaler())]
+    )
+
+    preprocessing = ColumnTransformer(
+        transformers = [
+            ('continuos', continuous_transformer, continuous_features)
+        ],
+        remainder = 'passthrough'
+    )
+
+    X_train_scaled = preprocessing.fit_transform(X_train)
+    #X_train_scaled = pd.DataFrame(X_train_scaled, columns=X_train.columns)
+    X_test_scaled = preprocessing.transform(X_test)
+    #X_test_scaled = pd.DataFrame(X_test_scaled, columns=X_test.columns)
+
+    feature_names = continuous_features + [col for col in X_train.columns if col not in continuous_features]
+    X_train_scaled = pd.DataFrame(X_train_scaled, columns=feature_names)
+    X_test_scaled = pd.DataFrame(X_test_scaled, columns=feature_names)
+
+    return X_train_scaled, X_test_scaled
